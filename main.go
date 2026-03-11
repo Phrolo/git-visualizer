71a24e63a141ede0f8aed352d54b62b710d018b1
Phrolo <cystrafe29@gmail.com>
2026-03-11 14:02:59 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+import (
+	"flag"
+	"fmt"
+)
+
+//scan given a path crawls it and its subfolders
+//searching for Git repositories
+
+//scan scans a new folder for Git repositories
+func scan(path string) {
+	fmt.Printf("Found folders:\n\n")
+	repositories := recursiveScanFolder(folder)
+	filePath := getDotFilePath()
+	addNewSliceElementsToFile(filePath, repositories)
+	fmt.Printf("\n\nSuccessfully added\n\n")
+}
+
+func stats(email string) {
+	print("stats")
+}
+
+func main() {
+	var folder string
+	var email string
+
+	flag.StringVar(&folder, "add", "", "add a new folder to scan for Git repos")
+	flag.StringVar(&email, "email", "your@email.com", "the email to scan")
+	flag.Parse()
+
+	if folder != "" {
+		scan(folder)
+		return
+	}
+
+	stats(email)
+}
