12664cfcc7466737869add72e9a8f3deb53dbb78
Phrolo <cystrafe29@gmail.com>
2026-03-12 14:11:41 +0800

    Added scanGitFolders(), recursiveScanFolder(), and getDotFilePath()

--- main.go
+++ main.go
@@ -1,14 +1,67 @@
 package main
+
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"os/user"
+	"strings"
 )
 
-//scan given a path crawls it and its subfolders
-//searching for Git repositories
+// returns a list of subfolders of folder ending with .git
+// returns the base folder of the repo, the .git folder parent
+// recursively searches in subfolders by passing an existing folders slice
+func scanGitFolders(folders []string, folder string) []string {
+	// trim the last '/'
+	folder = strings.TrimSuffix(folder, "/")
+	f, err := os.Open(folder)
+	if err != nil {
+		log.Fatal(err)
+	}
+	files, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-//scan scans a new folder for Git repositories
-func scan(path string) {
+	var path string
+	for _, file := range files {
+		if file.IsDir() {
+			path = folder + "/" + file.Name()
+			if file.Name() == ".git" {
+				path = strings.TrimSuffix(path, "/.git")
+				fmt.Println(path)
+				folders = append(folders, path)
+				continue
+			}
+			if file.Name() == "vendor" || file.Name() == "node_modules" {
+				continue
+			}
+			folders = scanGitFolders(folders, path)
+		}
+	}
+	return folders
+}
+
+func recursiveScanFolder(folder string) []string {
+	return scanGitFolders(make([]string, 0), folder)
+}
+
+func getDotFilePath() string {
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dotFile := usr.HomeDir + "/.gogitlocalstats"
+	return dotFile
+}
+
+// scan given a path crawls it and its subfolders
+// searching for Git repositories
+
+// scan scans a new folder for Git repositories
+func scan(folder string) {
 	fmt.Printf("Found folders:\n\n")
 	repositories := recursiveScanFolder(folder)
 	filePath := getDotFilePath()
